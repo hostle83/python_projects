6af471577bb5ca4c77efce4a414c65eafedc304b
hostle83 <holger.lenz@gmail.com>
2016-05-19 21:37:27 +0200

    Added days_between_dates function

---
+++ days_alive.py
@@ -0,0 +1,27 @@
+
+
+
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < 30:
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+
+        
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar, and the first date is not after
+       the second."""
+    counter = 0
+    while (year1, month1, day1) != (year2, month2, day2):
+        year1, month1, day1 = nextDay (year1, month1, day1)
+        counter = counter + 1
+    return counter
+

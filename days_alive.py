cb683b0249cb831e9d3e094d50c5e4de43167c24
hostle83 <holger.lenz@gmail.com>
2016-05-19 21:45:56 +0200

    Added help procedure "dateIsBefore" that checks if date1 is before date2

--- days_alive.py
+++ days_alive.py
@@ -13,6 +13,22 @@ def nextDay(year, month, day):
             return year, month + 1, 1
 
         
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """
+    Returns True if year1, month1, day1 is before year2, month2, day2;
+    otherwise return False
+    """
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False
+
+print dateIsBefore(2012, 12,12,2012, 12, 13)
+
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1

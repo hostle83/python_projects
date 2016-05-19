6e1055a65ff0e640d9126d9afa5e03b0c24efde3
hostle83 <holger.lenz@gmail.com>
2016-05-19 21:55:00 +0200

    Added assertion error for invalid inputs

--- days_alive.py
+++ days_alive.py
@@ -33,9 +33,13 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
        and year2/month2/day2. Assumes inputs are valid dates
        in Gregorian calendar, and the first date is not after
        the second."""
+    assert dateIsBefore(year1, month1, day1, year2, month2, day2)
     counter = 0
     while dateIsBefore(year1, month1, day1, year2, month2, day2):
         year1, month1, day1 = nextDay (year1, month1, day1)
         counter = counter + 1
     return counter
 
+
+print daysBetweenDates(2014, 1, 24, 2013, 6, 29)
+print daysBetweenDates(1912, 3, 29, 2012, 3, 29)
